f5eff5346ef4df9d8847b4968d9c7645853b0588
_caio.cp_ <93060384+CaioCesarP@users.noreply.github.com>
2022-01-26 11:06:58 -0300

    Add files via upload

--- pages/Copyright.js
+++ pages/Copyright.js
@@ -2,9 +2,9 @@ import { Typography, Link } from '@mui/material';
 
 function Copyright(props) {
     return (
-      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      <Typography sx={{m: '1rem' }} variant="body2" color="text.secondary" align="center" {...props}>
         {'Copyright © '}
-        <Link color="inherit" href="#">
+        <Link color="inherit" href="https://www.linkedin.com/in/profile-caio-c%C3%A9sar-link/">
           CaioCesarP
         </Link>{' '}
         {new Date().getFullYear()}
